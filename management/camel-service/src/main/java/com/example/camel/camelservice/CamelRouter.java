9a8d04ab1b8453dc832dc809d5ba2b237a54ebb3
Willem Jiang <willem.jiang@gmail.com>
2019-07-30 09:07:42 +0800

    Enabled the tracer on the camel context

--- management/camel-service/src/main/java/com/example/camel/camelservice/CamelRouter.java
+++ management/camel-service/src/main/java/com/example/camel/camelservice/CamelRouter.java
@@ -1,12 +1,16 @@
 package com.example.camel.camelservice;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CamelRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
+        // You can simple enable the tracing feature here
+        getContext().setTracing(true);
+
         from("timer:hello?period={{timer.period}}")
                 .routeId("MessageProducer")
                 .setBody().constant("Hello Camel!")
