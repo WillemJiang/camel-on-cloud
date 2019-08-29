5af1a023b737509c4c8c944e25701427484d1183
Willem Jiang <willem.jiang@gmail.com>
2019-08-29 17:00:56 +0800

    Added a demo to show to how to setup the services address of CXF client

--- legacy-proxy/webservice-client/webservice-camel-route/src/main/java/com/example/camel/demo/MyServiceBean.java
+++ legacy-proxy/webservice-client/webservice-camel-route/src/main/java/com/example/camel/demo/MyServiceBean.java
@@ -1,9 +1,9 @@
 package com.example.camel.demo;
 
 import org.apache.camel.Header;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.springframework.stereotype.Component;
 import sample.ws.service.Hello;
-import sample.ws.service.HelloService;
 
 import javax.annotation.PostConstruct;
 
@@ -13,9 +13,15 @@ public class MyServiceBean {
 
     @PostConstruct
     public void setServiceProxy() {
-        // create cxf client for invocation
-        HelloService service = new HelloService();
-        hello = service.getHelloPort();
+        // create cxf client for invocation with JAXWS API
+        // HelloService service = new HelloService();
+        // hello = service.getHelloPort();
+
+        // create the cxf client by using JaxWsFactoryBean, we can do some other configuration
+        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        factory.setAddress("http://localhost:8080/Service/Hello");
+        // as Hello.java is generated from WSDL, we don't need to specify the wsdl URL here.
+        hello = factory.create(Hello.class);
     }
 
     public String login(@Header("username") String username, @Header("password") String password) {
